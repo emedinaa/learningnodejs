ddd218a50bb1ecb7c650a75609ac76457baee6b9
Eduardo Medina <emedinaa@gmail.com>
2016-09-23 15:24:20 -0500

    initial commit

---
+++ bookapi/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+var app= express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req,res){
+	res.send('welcome to my API !');
+})
+
+app.listen(port, function(){
+	console.log('Gulp is running my app on PORT: '+port);
+	//console.log('Running on PORT: '+port);
+})
+
+//http://localhost:3000/
